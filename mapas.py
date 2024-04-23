11bb9bbf1a5766b64cbabc3f3e55ddd94d3df709
AranzaRmz12 <A01707746@tec.mx>
2024-04-22 19:47:30 -0600

    add uber data & edit mapas.py and uber.py

--- mapas.py
+++ mapas.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 
+# Comando para correr: python -m streamlit run mapas.py
+
 map_data = pd.DataFrame(
     np.random.randn(1000, 2) / [50, 50] + [20.612152, -100.402321],
     columns = ['lat', 'lon']
